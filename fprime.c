4647976dfe0aca5468e6d746892d4321289ca50d
R0arTech <chris.will.lyon@gmail.com>
2025-09-21 14:54:22 +0200

    update

--- fprime.c
+++ fprime.c
@@ -3,28 +3,28 @@
 
 int main(int ac, char **av)
 {
-    int i;
-    int num; 
+	int i;
+	int num; 
 
-    i = 2; 
-    if (ac == 2)
-    {
-        num = atoi(av[1]);
-        if (num == 1)
-            printf("1");
-        while (i <= num)
-        {
-            if (num % i == 0)
-            {
-                printf("%d", i);
-                if (num != i)
-                    printf("*");
-                num = num / i;
-            }
-            else
-                i++;
-        }
-    }
-    printf("\n");
-    return (0);
+	i = 2; 
+	if (ac == 2)
+	{
+		num = atoi(av[1]);
+		if (num == 1)
+			printf("1");
+		while (i <= num)
+		{
+			if (num % i == 0)
+			{
+				printf("%d", i);
+				if (num != i)
+					printf("*");
+				num = num / i;
+			}
+			else
+				i++;
+		}
+	}
+	printf("\n");
+	return (0);
 }
