8b13bfbef35eda21573bcb419aa590dd50f7327d
Christopher Lyon <clyon@c1r4s1.42luxembourg.lu>
2025-09-26 12:10:40 +0200

    update putnbr

--- Level_1/ft_strlen2.c
+++ Level_1/ft_strlen2.c
@@ -1,12 +1,6 @@
 #include <unistd.h> 
 
-int putnbr(int nbr)
-{
-    if (nbr > 9)
-        putnbr(nbr / 10);
-    write (1, &"0123456789"[nbr % 10], 1);
-    return (0);
-}
+
 
 int ft_strlen(char *str)
 {
---
+++ Level_1/putnbr.c
@@ -0,0 +1,9 @@
+
+#include <unistd.h>
+int putnbr(int nbr)
+{
+    if (nbr > 9)
+        putnbr(nbr / 10);
+    write (1, &"0123456789"[nbr % 10], 1);
+    return (0);
+}
