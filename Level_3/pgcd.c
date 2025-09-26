e61c971e3f0a0dc835e3a29695c7ac0e9792104b
Christopher Lyon <clyon@c1r4s1.42luxembourg.lu>
2025-09-26 12:32:21 +0200

    update

--- Level_3/pgcd.c
+++ Level_3/pgcd.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
+#include <unistd.h> 
 #include <stdlib.h>
 
 int main(int ac, char **av)
 {
-
-    if (ac == 3)
+    if(ac == 3)
     {
-        // I won't explain the whole thing
-	// but if we take 14 and 77 as input we would
-	// have the following (each iteration separated by ;)
-	// 14; 14; 14; 14; 14; 14; 7
-	// 77; 63; 49; 35; 21; 7;  7
-        int number1 = atoi(av[1]);
-        int number2 = atoi(av[2]);
-        
-        if (number1 > 0 && number2 > 0)
+        int number1;
+        int number2;
+
+        number1 = atoi(av[1]);
+        number2 = atoi(av[2]);
+
+        if(number1 > 0 && number2 > 0)
         {
-            while (number1 != number2)
+            while(number1 != number2)
             {
-                if (number1 > number2)
-                    number1 = number1 - number2;
-                else
-                    number2 = number2 - number1;
+                if(number1 > number2)
+                    number1 -= number2;
+                else if(number2 > number1)
+                    number2 -= number1;
             }
-            printf("%d", number1);
+        printf("%d", number1);
         }
     }
     printf("\n");
