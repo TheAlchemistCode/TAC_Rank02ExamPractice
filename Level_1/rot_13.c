75b14e4e4ff019ad654fa8c31bc36e3acbdfa57a
Chris Lyon <chris.q.lyon@gmail.com>
2025-09-27 15:12:47 +0200

    improved rot13

--- Level_1/rot_13.c
+++ Level_1/rot_13.c
@@ -4,18 +4,19 @@ int main(int ac, char *av[])
 {
     if (ac == 2)
     {
-        int i;
+        int i = 0; 
 
-        i = 0;
         while (av[1][i])
         {
-            if ((av[1][i] >= 65 && av[1][i]) <= 77 || (av[1][i] >=97 && av[1][i] <= 109))
+            if ((av[1][i] >= 'A' && av[1][i] <= 'M') || (av[1][i] >= 'a' && av[1][i] <= 'm'))
                 av[1][i] += 13;
-            else if ((av[1][i] >= 78 && av[1][i]) <= 90 || (av[1][i] >=110 && av[1][i] <= 122))
+            else if ((av[1][i] >= 'N' && av[1][i] <= 'Z') || (av[1][i] >= 'n' && av[1][i] <= 'z'))
                 av[1][i] -= 13;
-            write (1, &av[1][i], 1);
+            
+            write(1, &av[1][i], 1);
             i++;
         }
     }
     write(1, "\n", 1);
+    return (0);
 }
