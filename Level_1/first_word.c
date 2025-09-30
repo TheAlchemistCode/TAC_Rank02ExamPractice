60b6def30cec3c9a721a1fc60634d8b165165c9d
Christopher Lyon <clyon@c3r2s1.42luxembourg.lu>
2025-09-30 17:35:56 +0200

    update after exam

--- Level_1/first_word.c
+++ Level_1/first_word.c
@@ -1,21 +1,43 @@
 #include <unistd.h>
 
-int main(int argc,char **av)
+/*passable version*/
+#include <unistd.h>
+int	main(int ac, char **av)
 {
-	int i;
-
-	i = 0;
-	if (argc == 2)
+	if (ac == 2)
 	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-		i++;
-		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+		int i = 0;
+		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i++;
+		while (av[1][i] && (av[1][i] >= 33 && av[1][i] <= 126))
 		{
 			write(1, &av[1][i], 1);
 			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return(0);
-	
+	return (0);
+}
+
+/*fails*/
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 0;
+		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i++;
+		while (av[1][i])
+		{
+			if (av[1][i] >= 33 && av[1][i] <= 126)
+			{
+				write(1, &av[1][i], 1);
+				i++;
+			}
+			else
+				break;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
