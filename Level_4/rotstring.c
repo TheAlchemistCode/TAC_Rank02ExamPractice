90aa72d4eb7cf2b252ac9c83cff3aa150390ad5c
Christopher Lyon <clyon@c1r4s1.42luxembourg.lu>
2025-10-06 17:19:53 +0200

    updating rotstring

--- Level_4/rotstring.c
+++ Level_4/rotstring.c
@@ -1,56 +1,57 @@
-#include <unistd.h>
+#include <unistd.h> 
 
-void	rotstring(char *str)
+void rotstring(char *str)
 {
-	int	i;
-	int	start;
-	int	end;
+    int start;
+    int end;
+    int i; 
 
-	i = 0;
-	// Skip leading spaces and tabs
-	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
-		i++;
-	
-	// Remember where first word starts
-	start = i;
-	
-	// Find end of first word
-	while (str[i] && str[i] != ' ' && str[i] != '\t')
-		i++;
-	
-	// Remember where first word ends
-	end = i;
-	
-	// Skip spaces after first word
-	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
-		i++;
-	
-	// Print everything AFTER the first word
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-	
-	// If there was something after first word, add space
-	if (str[end])
-		write(1, " ", 1);
-	
-	// Print the first word at the end
-	i = start;
-	while (i < end)
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+    i = 0; 
+    while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+        i++;
+    start = i; 
+
+    while (str[i] && str[i] != ' ' && str[i] != '\t')
+        i++;
+    end = i;
+
+    while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+        i++;
+    
+    if (str[i])
+    {
+        while (str[i])
+        {
+            if (str[i] == ' ' || str[i] == '\t')
+            {
+                while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+                    i++;
+                if (str[i])
+                    write (1, " ", 1); // write space only if more chars follow
+            }
+            else
+            {
+                write(1, &str[i], 1);
+                i++;
+            }
+        }
+        write (1, " ", 1);
+    }
+    
+    i = start; 
+    while (i < end)
+    {
+        write (1, &str[i], 1);
+        i++;
+    }
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac == 2)
-	{
-		rotstring(av[1]);
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (ac >= 2)
+    {
+        rotstring(av[1]);
+    }
+    write (1, "\n", 1);
+    return (0);
 }
