bb0d9726d08b4b0aa0c0868c87138dceec4ed374
Chris Lyon <chris.q.lyon@gmail.com>
2025-10-13 11:42:57 +0200

    updating

---
+++ Level_2/max.c
@@ -0,0 +1,25 @@
+int max(int* array, unsigned int len)
+{
+    if (len == 0)
+        return (0);
+    
+    int maxnum = array[0];    // Start with first element, not 0
+    unsigned int i = 1;     // Start from second element
+    
+    while (i < len)
+    {
+        if (array[i] > maxnum)
+            maxnum = array[i];
+        i++;
+    }
+    return (maxnum);
+}
+
+#include <stdio.h>
+
+#include <stdio.h>
+int main(void)
+{
+    int array[] = {3,4,56,7,2};
+    printf("%d", max(array, 5));
+}
