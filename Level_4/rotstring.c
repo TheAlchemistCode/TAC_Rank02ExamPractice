379d27f05282c2e7777884704fcb5711522a68e5
R0arTech <chris.will.lyon@gmail.com>
2025-10-16 16:54:15 +0200

    updating

--- Level_4/rotstring.c
+++ Level_4/rotstring.c
@@ -7,41 +7,52 @@ void rotstring(char *str)
     int i; 
 
     i = 0; 
+    // Skip leading whitespace
     while (str[i] && (str[i] == ' ' || str[i] == '\t'))
         i++;
     start = i; 
 
+    // Find end of first word
     while (str[i] && str[i] != ' ' && str[i] != '\t')
         i++;
     end = i;
 
+    // Skip whitespace after first word
     while (str[i] && (str[i] == ' ' || str[i] == '\t'))
         i++;
     
+    // Print all words EXCEPT the first (if they exist)
     if (str[i])
     {
+        int space_flag = 0;
+        
         while (str[i])
         {
             if (str[i] == ' ' || str[i] == '\t')
             {
+                space_flag = 1;  // Remember we saw whitespace
                 while (str[i] && (str[i] == ' ' || str[i] == '\t'))
-                    i++;
-                if (str[i])
-                    write (1, " ", 1); // write space only if more chars follow
+                    i++;  // Skip all consecutive whitespace
             }
             else
             {
-                write(1, &str[i], 1);
+                if (space_flag)
+                {
+                    write(1, " ", 1);  // Print single space
+                    space_flag = 0;    // Reset flag
+                }
+                write(1, &str[i], 1);  // Print character
                 i++;
             }
         }
-        write (1, " ", 1);
+        write(1, " ", 1);  // Space before first word
     }
     
+    // Print the first word
     i = start; 
     while (i < end)
     {
-        write (1, &str[i], 1);
+        write(1, &str[i], 1);
         i++;
     }
 }
@@ -52,6 +63,6 @@ int main(int ac, char **av)
     {
         rotstring(av[1]);
     }
-    write (1, "\n", 1);
+    write(1, "\n", 1);
     return (0);
 }
