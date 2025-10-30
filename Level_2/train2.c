cba18edc2c1bff5f1a7b0000e2651a4a4eceb584
R0arTech <chris.will.lyon@gmail.com>
2025-10-30 12:02:15 +0100

    updating bits

--- Level_2/train2.c
+++ Level_2/train2.c
@@ -1,2 +1,12 @@
 
 unsigned char swap_bits(unsigned char octet)
+{
+    return((octet >> 4) | (octet << 4));
+}
+
+#include <stdio.h>
+int main(void)
+{
+    unsigned char octet = 'F';
+    printf("%c", swap_bits(octet));
+}
